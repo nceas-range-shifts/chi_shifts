12c530058a0f57cc50d6c823d7a84df674b83e6d
Casey O'Hara <casey.c.ohara@gmail.com>
2026-05-25 02:45:29 +0000

    feat: test different methods of loading and wrangling
    Trying dplyr, data.table, collapse, and duckdb for various reading, filtering, selecting, and summarizing for timing.

--- common_fxns.R
+++ common_fxns.R
@@ -14,3 +14,139 @@ get_aquax_meta <- function(meta = 'META_02122025.Rdata', sdm = TRUE) {
     filter(sdm == 'SDM')
   return(meta_clean)
 }
+
+get_spp_traits <- function() {
+  ### run data mgmt scripts first!
+  traits_dir <- here::here('data/spp_vuln_framework_traits')
+  junction_df <- data.table::fread(file.path(traits_dir, 'spp_traits_junction.csv'))
+  traitval_df <- data.table::fread(file.path(traits_dir, 'spp_traits_trait_val_lookup.csv'))
+  taxa_df     <- data.table::fread(file.path(traits_dir, 'spp_traits_taxa_lookup.csv'))
+  gapfill_df  <- data.table::fread(file.path(traits_dir, 'spp_traits_gapfill_levels.csv'))
+  df <- junction_df %>%
+    left_join(taxa_df, by = 'aphia_id') %>%
+    left_join(traitval_df, by = 'trait_id') %>%
+    left_join(gapfill_df, by = 'gf_match_id') %>%
+    select(-trait_id, -gf_match_id)
+  return(df)
+}
+
+get_spp_vuln <- function() {
+  ### run data mgmt scripts first!
+  vuln_dir <- here::here('data/spp_vuln_framework_scores')
+  junction_df <- data.table::fread(file.path(vuln_dir, 'spp_vuln_junction.csv'))
+  taxa_df     <- data.table::fread(file.path(vuln_dir, 'spp_vuln_taxa_lookup.csv'))
+  stressor_df <- data.table::fread(file.path(vuln_dir, 'spp_vuln_stressor_lookup.csv'))
+  df <- junction_df %>%
+    left_join(taxa_df, by = 'aphia_id') %>%
+    left_join(stressor_df, by = 'vuln_str_id') %>%
+     select(-vuln_str_id)
+  return(df)
+}
+
+get_sdm_duckdb <- function(aphia_id, 
+                    scenario = c('Current', 
+                                 'RCP26_2050', 'RCP26_2100',
+                                 'RCP45_2050', 'RCP45_2100', 
+                                 'RCP85_2050', 'RCP85_2100'),
+                    xvec = NULL,
+                    apply_thresh = TRUE) {
+
+  ### define internal function:
+  process_one_sdm <- function(id, scenario, apply_thresh) {
+    filestem <- here_aquax('SDM/FINAL_EMSDM_EMMEAN_SP_%s.parquet')
+
+    ### open connection, select/filter
+    con <- duckdb::dbConnect(duckdb::duckdb())
+    
+    query_str <- paste0(
+      "SELECT x, y, ", paste(scenario, collapse = ', cutoff, '),
+      " FROM read_parquet('", sprintf(filestem, id), "')"
+    )
+    if(!is.null(xvec)) {
+      ### tack on WHERE clause
+      query_str <- paste0(
+        query_str,
+        " WHERE x IN (", paste(xvec, collapse = ', ') , ")"
+        )
+    }
+    df <- DBI::dbGetQuery(con, query_str) %>%
+      janitor::clean_names() %>%
+      mutate(aphia_id = id)
+    
+    ### close connection!
+    duckdb::dbDisconnect(con, shutdown = TRUE)
+
+    if(apply_thresh) {
+      scen = tolower(scenario)
+      df <- df %>%
+        mutate(across(all_of(scen), ~ifelse(.x < cutoff, NA, 1))) %>%
+        filter(!if_all(all_of(scen), is.na))
+    }
+    return(df)
+  }
+  ### if single ID, process directly; otherwise, parallel:
+  if(length(aphia_id) == 1) {
+    sdm_df <- process_one_sdm(id = aphia_id, 
+                              scenario = scenario, 
+                              apply_thresh = apply_thresh)
+  } else {
+    ### set # cores based on # ids, up to some max (20)
+    n_cores <- min(length(aphia_id), 20)
+    sdm_list <- parallel::mclapply(
+      mc.cores = n_cores,
+      X = aphia_id,
+      FUN = process_one_sdm,
+      scenario = scenario, apply_thresh = apply_thresh
+    )
+    sdm_df <- data.table::rbindlist(sdm_list)
+  }
+  return(sdm_df)
+}
+
+get_sdm_dplyr <- function(aphia_id, 
+                          scenario = c('Current', 
+                                       'RCP26_2050', 'RCP26_2100',
+                                       'RCP45_2050', 'RCP45_2100', 
+                                       'RCP85_2050', 'RCP85_2100'),
+                          xvec = NULL,
+                          apply_thresh = TRUE) {
+  
+  ### define internal function:
+  process_one_sdm <- function(id, scenario, apply_thresh) {
+    scen <- tolower(scenario)
+    filestem <- here_aquax('SDM/FINAL_EMSDM_EMMEAN_SP_%s.parquet')
+    df <- arrow::read_parquet(sprintf(filestem, id)) %>%
+      janitor::clean_names() %>%
+      select(x, y, all_of(scen), cutoff) %>%
+      mutate(aphia_id = id)
+    if(!is.null(xvec)) { 
+      df <- df %>%
+        filter(x %in% xvec)
+    }
+    if(apply_thresh) {
+      df <- df %>%
+        mutate(across(all_of(scen), ~ifelse(.x < cutoff, NA, 1))) %>%
+        filter(!if_all(all_of(scen), is.na))
+    }
+    return(df)
+  }
+  ### if single ID, process directly; otherwise, parallel:
+  if(length(aphia_id) == 1) {
+    sdm_df <- process_one_sdm(id = aphia_id, 
+                              scen = scenario, 
+                              apply_thresh = apply_thresh)
+  } else {
+    ### set # cores based on # ids, up to some max (20)
+    n_cores <- min(length(aphia_id), 20)
+    sdm_list <- parallel::mclapply(
+      mc.cores = n_cores,
+      X = aphia_id,
+      FUN = process_one_sdm,
+      scen = scenario, apply_thresh = apply_thresh
+    )
+    sdm_df <- data.table::rbindlist(sdm_list)
+  }
+  return(sdm_df)
+}
+
+get_sdm <- get_sdm_dplyr
